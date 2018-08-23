2fc41c8847ccf2b20719d839009e84748896e5be
MAF <maf@DES000447s-iMac.local>
2018-08-23 21:02:47 +0530

    results reducer

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,32 +1,11 @@
-import * as actionTypes from './actions';
+import * as actionTypes from '../actions';
 
 const initialState = {
-    counter: 0,
     results: []
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.INCREMENT:
-            return {
-                ...state,
-                counter: state.counter + 1
-            }
-        case actionTypes.DECREMENT:
-            return {
-                ...state,
-                counter: state.counter - 1
-            }
-        case actionTypes.ADD:
-            return {
-                ...state,
-                counter: state.counter + action.val
-            }
-        case actionTypes.SUBTRACT:
-            return {
-                ...state,
-                counter: state.counter - action.val
-            }
         case actionTypes.STORE_RESULT:
             return {
                 ...state,
